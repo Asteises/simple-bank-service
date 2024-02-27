c8e832e62adba04aba56b4f21dbe0372765e0b26
Asteises <p.nikitaev@gmail.com>
2024-02-27 14:40:37 +0300

    init commit

---
+++ src/main/java/ru/asteises/simplebankservice/SimpleBankServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.asteises.simplebankservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBankServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBankServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/asteises/simplebankservice/SimpleBankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.asteises.simplebankservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
